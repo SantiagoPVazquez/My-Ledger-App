27ffa5e0e6e304c6bcd12ead8e5f7748cd0ddd9a
Santiago Pereyra Vázquez <santiago.pvazquez@gmail.com>
2023-01-02 03:42:53 -0600

    All commands and flags working

--- src/my-ledger.py
+++ src/my-ledger.py
@@ -51,26 +51,81 @@ def readData(filePath):
             account = line.split()[0]
             if "$" in line:
                 amount = float(line.split("$")[1].strip().replace(",",""))
+                unit = "$"
+            elif any(char.isdigit() for char in line):#len(line.split()) > 1:
+                for i in range(len(line.split())):
+                    if line.split()[i].isnumeric():
+                        amount = float(line.split()[i])
+                        unit = line.split()[i+1]
             else:
                 amount = -float(amount)
 
             # Appending the account name and amount to the last entry
             data[entries]["transactions"].append({
                 "account": account,
-                "amount": amount
+                "amount": amount,
+                "unit": unit
             })
     return data
 
 ############### Defining the "sort" function ###############
-def sortData(data):
-    ...
+def sortData(data, sorted):
+    # New list to save the data
+    sortedData = [None]*len(data)
+
+    if sorted == "date":
+        
+        dateList = []
+        for i in range(len(data)):
+            dateList.append(data[i]["date"])
+
+        for i in range(len(data)):
+            maxDateind = dateList.index(max(dateList))
+            dateList.pop(maxDateind)
+            sortedData[i] = data.pop(maxDateind)
+        
+        return sortedData
+
+    elif sorted == "amount":
+
+        amountList = {}
+        amounts = []
+        for i in range(len(data)):
+            for j in range(len(data[i]["transactions"])):
+                amounts.append(data[i]["transactions"][j]["amount"])
+                amountList[i] = amounts
+            amounts = []
+
+        for i in range(len(data)):
+            maxAmountind = list(filter(lambda x: amountList[x] == max(amountList.values()), amountList))[0]
+            amountList.pop(maxAmountind)
+            sortedData[i] = data[maxAmountind]
+
+        return sortedData
+
+    else: 
+        return data
 
 ############### Defining the "prices-db" function ###############
-def pricesDatabase(data):
-    ...
+def pricesDatabase(data, pricesFile):
+    priceList = {}
+    with open(pricesFile, "r") as prices:
+        for line in prices:
+            if len(line.split()) > 2:
+                priceList[line.split()[-2]] = float(line.split()[-1].replace("$","").replace(",",""))
+    
+    for i in range(len(data)):
+        for j in range(len(data[i]["transactions"])):
+            if data[i]["transactions"][j]["unit"] in priceList:
+                data[i]["transactions"][j]["amount"] = data[i]["transactions"][j]["amount"] * priceList[data[i]["transactions"][j]["unit"]]
+                data[i]["transactions"][j]["unit"] = "$"
+    
+    return data
+                    
+
 
 ############### Defining the "balance" function ###############
-def balanceReport(data, sorted = False, prices = None):
+def balanceReport(data):
     balanceBook = {}
     for i in range(len(data)):
         for j in range(len(data[i]["transactions"])):
@@ -99,17 +154,32 @@ def balanceReport(data, sorted = False, prices = None):
     print("------------------------\n","\t{:^10.2f}".format(sum))
 
 ############### Defining the "register" function ###############
-def registerReport(data, sorted = False, prices = None):
-    ...
+def registerReport(data):
+
+    sum = 0
+    for i in range(len(data)):
+        temp = data[i]["entry"].strip()
+        for j in range(len(data[i]["transactions"])):
+            sum += data[i]["transactions"][j]["amount"]
+            if data[i]["transactions"][j]["unit"] == "$":
+                print("{:<40}\t{:<25}\t${:^20.2f}\t{:^20.2f}".format(temp,data[i]["transactions"][j]["account"],data[i]["transactions"][j]["amount"],sum))
+            else:
+                print("{:<40}\t{:<25}\t{:^20.2f}{:<}\t{:^20.2f}".format(temp,data[i]["transactions"][j]["account"],data[i]["transactions"][j]["amount"],data[i]["transactions"][j]["unit"],sum))
+
+            temp = ""
 
 ############### Defining the "print" function ###############
-def printReport(data, sorted = False, prices = None):
+def printReport(data):
     # Main loop to print every main entry from the journal
     for i in range(len(data)):
         print(data[i]["entry"])
         # Secondary loop to print all the transactions for each entry
         for j in range(len(data[i]["transactions"])):
-            print("\t{:<40}${:^10.2f}\n".format(data[i]["transactions"][j]["account"], data[i]["transactions"][j]["amount"]))
+            if data[i]["transactions"][j]["unit"] == "$":
+                print("\t{:<40}{}{:^10.2f}\n".format(data[i]["transactions"][j]["account"],data[i]["transactions"][j]["unit"], data[i]["transactions"][j]["amount"]))
+            elif data[i]["transactions"][j]["unit"] != "$":
+                print("\t{:<40}{:^10.2f}{}\n".format(data[i]["transactions"][j]["account"],data[i]["transactions"][j]["amount"], data[i]["transactions"][j]["unit"]))
+    
 
 ############### Main ###############
 if __name__ == "__main__":
@@ -124,6 +194,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--sort", "-S",
         type = str,
+        dest = "sort",
         choices = ["date", "amount"],
         help = "Sort report by date or amount"
     )
@@ -146,19 +217,29 @@ if __name__ == "__main__":
     # Saved arguments
     parsed_args = parser.parse_args()
 
+    # Reading data
+    data = readData(parsed_args.file[0])
+
+    # Modifying data if sort and price-db flag is used 
+    if parsed_args.sort != None and parsed_args.priceDatabase != None:
+        data = pricesDatabase(data, parsed_args.priceDatabase[0])
+        data = sortData(data, parsed_args.sort)
+    # Modifying data if only sort flag is used
+    elif parsed_args.sort != None and parsed_args.priceDatabase == None:
+        data = sortData(data, parsed_args.sort)
+    # Modifying data if only price-db flag is used
+    elif parsed_args.priceDatabase != None and parsed_args.sort == None:
+        data = pricesDatabase(data, parsed_args.priceDatabase[0])
+
     # Executing functions depending on the command
     # Calling the "balance" function
     if parsed_args.command == "bal":
-        readData(parsed_args.file[0])
-        balanceReport(data, parsed_args.sort, parsed_args.priceDatabase)
+        balanceReport(data)
 
     # Calling the "register" function
     elif parsed_args.command == "reg":
-        readData(parsed_args.file[0])
-        registerReport(data, parsed_args.sort, parsed_args.priceDatabase)
+        registerReport(data)
 
     # Calling the "print" function
     elif parsed_args.command == "print":
-        readData(parsed_args.file[0])
-        printReport(data, parsed_args.sort, parsed_args.priceDatabase)
-    
+        printReport(data)
